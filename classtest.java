03f5fb0adb1e60e404d049f87b3dd8c100d22a43
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-07-08 08:59:36 +0530

    Add files via upload

---
+++ classtest.java
@@ -0,0 +1,16 @@
+package fibonacciseries;
+
+public class classtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b;
+a=10;
+b=(a==1)?20:30;
+System.out.println("Value of b is:"+b);
+b=(a==10)?20:30;
+System.out.println("Value of b is:"+b);
+
+	}
+
+}
